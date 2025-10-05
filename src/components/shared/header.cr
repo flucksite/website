bd27ac92b51d8d915c74ab015beb1f0f510beed7
Wout <hi@wout.codes>
2025-10-05 13:20:31 +0200

    feat: add site header

---
+++ src/components/shared/header.cr
@@ -0,0 +1,20 @@
+class Shared::Header < BaseComponent
+  def render
+    header class: "header" do
+      link to: Home::Index do
+        span "Fluck", class: "visually-hidden"
+        inline_svg("fluck-logo-dark-large-horizontal.svg")
+      end
+      nav do
+        ul role: "list" do
+          li do
+            link r("global.menu.item.about").t, to: About::Index
+          end
+          li do
+            link r("global.menu.item.waitlist").t, to: About::Index
+          end
+        end
+      end
+    end
+  end
+end

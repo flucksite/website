2a2695497c83f83e0c7c3054a41450ac8c2e72b1
Wout <hi@wout.codes>
2025-10-12 11:28:09 +0200

    feat: add subscription operation

--- src/operations/mailing_list_subscription.cr
+++ src/operations/mailing_list_subscription.cr
@@ -3,8 +3,22 @@ class MailingListSubscription < Avram::Operation
   attribute email : String
 
   def run
+    validate_required email
     validate_format_of email, with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
+    validate_required list
 
-    # Subscribe user
+    subscribe_user
+  end
+
+  private def subscribe_user
+    EmailOctopus::Contact.create_or_update(
+      api_client,
+      list_id: list.value.to_s,
+      email_address: email.value.to_s
+    )
+  end
+
+  private def api_client
+    EmailOctopus::Client.new(ENV.fetch("EMAIL_OCTOPUS_API_KEY"))
   end
 end

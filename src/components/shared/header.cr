b91426cda5322a8f3198b3570c30c1c14c3e51c7
Wout <hi@wout.codes>
2025-10-16 14:31:12 +0200

    refactor: simplify header styling

--- src/components/shared/header.cr
+++ src/components/shared/header.cr
@@ -1,13 +1,13 @@
 class Shared::Header < BaseComponent
   def render
     header class: "header wrapper" do
-      div class: "repel", data_wrapper_type: "inner" do
+      div class: "repel" do
         link to: Home::Index do
           span "Fluck", class: "visually-hidden"
           inline_svg("fluck-logo-dark-large-horizontal-transparent.svg")
         end
         nav do
-          ul role: "list", class: "header__menu" do
+          ul class: "menu", data_direction: "horizontal" do
             li do
               link r("global.menu.item.about").t, to: About::Index
             end

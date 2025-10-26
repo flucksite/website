b8efed13e4d5e16d7d1290923f7a66bcacf27ef7
Wout <hi@wout.codes>
2025-10-26 19:45:23 +0100

    fix: disable debug reports in production only

--- config/error_handler.cr
+++ config/error_handler.cr
@@ -1,4 +1,4 @@
 Lucky::ErrorHandler.configure do |settings|
-  # settings.show_debug_output = !LuckyEnv.production?
-  settings.show_debug_output = false
+  settings.show_debug_output = !LuckyEnv.production?
+  # settings.show_debug_output = false
 end

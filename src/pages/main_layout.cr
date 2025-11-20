f223d56480904e14d65b82b21b5ec522404a5575
Wout <hi@wout.codes>
2025-11-20 08:56:15 +0100

    chore: remove wrapper around cutouts svg

--- src/pages/main_layout.cr
+++ src/pages/main_layout.cr
@@ -29,9 +29,7 @@ abstract class MainLayout
         x_data: "theme(#{current_theme})",
         ":data_theme": "theme" do
         skip_link
-        div class: "visually-hidden" do
-          inline_svg("clip-path-shapes-03.svg")
-        end
+        inline_svg("clip-path-shapes-03.svg", class: "visually-hidden")
         mount Shared::Header
         mount Shared::FlashMessages, context.flash
         main id: "main_content" do

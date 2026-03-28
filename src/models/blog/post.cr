6a516b8b3dccc2e299274d0f201f42a0f3b590ee
Wout <hi@wout.codes>
2026-03-28 20:41:17 +0100

    refactor: use marquery setup

--- src/models/blog/post.cr
+++ src/models/blog/post.cr
@@ -1,11 +1,3 @@
-require "json"
-
 struct Blog::Post
-  include JSON::Serializable
-
-  getter title : String
-  getter description : String?
-  getter content : String
-  getter date : Time
-  getter active : Bool = false
+  include Marquery::Model
 end
--- src/queries/blog/post_query.cr
+++ src/queries/blog/post_query.cr
@@ -1,21 +1,5 @@
 class Blog::PostQuery
-  macro load!
-    {%
-      posts = run("../../run_macros/posts_parser")
+  include Marquery
 
-      unless posts.starts_with?('[')
-        raise "Failed to parse blog posts: " + posts
-      end
-    %}
-
-    POSTS = Array(Blog::Post).from_json({{ posts.stringify }})
-  end
-
-  def initialize
-    @posts = POSTS
-  end
-
-  def all
-    @posts.sort_by(&.date).reverse
-  end
+  model Blog::Post
 end

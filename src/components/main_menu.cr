aa3ecda47a58148213466d40c0a880990663ff80
Wout <hi@wout.codes>
2026-04-09 19:13:57 +0200

    feat: add menu link for blog

--- src/components/main_menu.cr
+++ src/components/main_menu.cr
@@ -1,5 +1,5 @@
 class MainMenu < BaseComponent
-  MENU_ITEMS = %i[about waitlist]
+  MENU_ITEMS = %i[blog about waitlist]
 
   def render
     nav class: "main-menu", aria_labelledby: "main_nav_label" do

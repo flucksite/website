70b426fd4329b320e952ef1906826599cc396e33
Wout <hi@wout.codes>
2026-04-11 12:03:31 +0200

    feat: add tag list page helper mixin

---
+++ src/components/mixins/tag_listable.cr
@@ -0,0 +1,16 @@
+module TagListable
+  def render_tags(tags : Array(String), current_tag : String? = nil, &)
+    return if tags.empty?
+
+    ul class: "tag-list", role: "list" do
+      tags.each do |tag|
+        li do
+          link tag,
+            to: yield(tag == current_tag ? nil : tag, tag),
+            class: "tag",
+            aria_current: (tag == current_tag).to_s
+        end
+      end
+    end
+  end
+end

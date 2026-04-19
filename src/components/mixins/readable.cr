b518286a6a0e9e5b49635980987841baf7bafd32
Wout <hi@wout.codes>
2026-04-19 12:22:06 +0200

    chore: tweak reading time calculation

--- src/components/mixins/readable.cr
+++ src/components/mixins/readable.cr
@@ -1,9 +1,10 @@
 module Readable
-  WORDS_PER_MINUTE = 250
+  WORDS_PER_MINUTE  = 225
+  ROUND_UP_TOLERANCE = 0.2
 
   def reading_time_in_minutes(content : String) : Int32
     word_count = content.split(/\s+/).count { |word| !word.empty? }
-    minutes = (word_count / WORDS_PER_MINUTE.to_f).round.to_i
+    minutes = ((word_count / WORDS_PER_MINUTE.to_f) - ROUND_UP_TOLERANCE).ceil.to_i
     Math.max(1, minutes)
   end
 end
